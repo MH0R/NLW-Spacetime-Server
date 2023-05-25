31793a9584013db2ac050f3a346ee1be6f353082
Thalles Oliveira <66585100+MH0R@users.noreply.github.com>
2023-05-25 01:50:26 -0300

    Server commit

---
+++ src/auth.auth.d.ts
@@ -0,0 +1,12 @@
+// fastify-jwt.d.ts
+import '@fastify/jwt'
+
+declare module '@fastify/jwt' {
+  export interface FastifyJWT {
+    user: {
+      sub: string
+      name: string
+      avatarUrl: string
+    } // user type is return type of `request.user` object
+  }
+}
